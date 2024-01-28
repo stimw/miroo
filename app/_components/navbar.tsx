0def74872760ce9ff9cf9071ec43a002d30cd1a5
stimw <me@stimw.com>
2024-01-28 13:45:00 +0800

    feat: navbar & org search

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -6,15 +6,17 @@ import {
   useOrganization,
 } from '@clerk/nextjs';
 
-// import { SearchInput } from './search-input';
-// import { InviteButton } from './invite-button';
+import { SearchInput } from './search-input';
+import { InviteButton } from './invite-button';
 
 export const Navbar = () => {
   const { organization } = useOrganization();
 
   return (
     <div className="flex items-center gap-x-4 p-5">
-      <div className="hidden lg:flex lg:flex-1">{/* <SearchInput /> */}</div>
+      <div className="hidden lg:flex lg:flex-1">
+        <SearchInput />
+      </div>
       <div className="block lg:hidden flex-1">
         <OrganizationSwitcher
           hidePersonal
@@ -39,7 +41,7 @@ export const Navbar = () => {
           }}
         />
       </div>
-      {/* {organization && <InviteButton />} */}
+      {organization && <InviteButton />}
       <UserButton />
     </div>
   );

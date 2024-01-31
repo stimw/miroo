f62a068a3b365187e9f44e3a841c59d0592775f0
stimw <zhiwai98@gmail.com>
2024-01-31 18:26:00 +0800

    feat: toast when create board

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ConvexClientProvider } from '@/providers/convex-client-provider';
 import { DashboardLayout } from '@/app/_components/dashboard-layout';
+import { Toaster } from '@/components/ui/sonner';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +21,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ConvexClientProvider>
-          <DashboardLayout>{children}</DashboardLayout>
+          <DashboardLayout>
+            <Toaster />
+            {children}
+          </DashboardLayout>
         </ConvexClientProvider>
       </body>
     </html>

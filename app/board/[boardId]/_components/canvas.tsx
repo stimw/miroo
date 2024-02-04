903f4227816f8db2431e3d53b3a8f12433406d8c
stimw <me@stimw.com>
2024-02-04 23:46:00 +0800

    feat: liveblocks cursor presence

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,12 +1,19 @@
 'use client';
 
-import { useState } from 'react';
-
-import { useHistory, useCanUndo, useCanRedo } from '@/liveblocks.config';
-import { CanvasMode, CanvasState } from '@/types/canvas';
+import { useCallback, useState } from 'react';
 import { Info } from './info';
 import { Toolbar } from './toolbar';
 import { Participants } from './participants';
+
+import {
+  useHistory,
+  useCanUndo,
+  useCanRedo,
+  useMutation,
+} from '@/liveblocks.config';
+import { pointerEventToCanvasPoint } from '@/lib/utils';
+import { Camera, CanvasMode, CanvasState } from '@/types/canvas';
+import { CursorsPresence } from './cursors-presence';
 
 interface CanvasProps {
   boardId: string;
@@ -21,6 +28,30 @@ export const Canvas = ({ boardId }: CanvasProps) => {
   const canUndo = useCanUndo();
   const canRedo = useCanRedo();
 
+  const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
+
+  const onWheel = useCallback((e: React.WheelEvent) => {
+    setCamera((camera) => ({
+      x: camera.x - e.deltaX,
+      y: camera.y - e.deltaY,
+    }));
+  }, []);
+
+  const onPointerMove = useMutation(
+    ({ setMyPresence }, e: React.PointerEvent) => {
+      e.preventDefault();
+
+      const current = pointerEventToCanvasPoint(e, camera);
+
+      setMyPresence({ cursor: current });
+    },
+    []
+  );
+
+  const onPointerLeave = useMutation(({ setMyPresence }) => {
+    setMyPresence({ cursor: null });
+  }, []);
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
@@ -33,6 +64,16 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         undo={history.undo}
         redo={history.redo}
       />
+      <svg
+        className="h-[100vh] w-[100vw]"
+        onWheel={onWheel}
+        onPointerMove={onPointerMove}
+        onPointerLeave={onPointerLeave}
+      >
+        <g>
+          <CursorsPresence />
+        </g>
+      </svg>
     </main>
   );
 };

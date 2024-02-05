8036aa30769aa4cfd683de397bd58df613052077
stimw <zhiwai98@gmail.com>
2024-02-05 11:07:00 +0800

    feat: liveblocks insert rectangle layer

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,4 +1,10 @@
-import { createClient } from '@liveblocks/client';
+import {
+  createClient,
+  LiveList,
+  LiveMap,
+  LiveObject,
+} from '@liveblocks/client';
+import { Layer, Color } from '@/types/canvas';
 import { createRoomContext, createLiveblocksContext } from '@liveblocks/react';
 
 const client = createClient({
@@ -51,6 +57,7 @@ const client = createClient({
 // `user.presence` property. Must be JSON-serializable.
 type Presence = {
   cursor: { x: number; y: number } | null;
+  selection: string[];
 };
 
 // Optionally, Storage represents the shared document that persists in the
@@ -58,8 +65,8 @@ type Presence = {
 // LiveList, LiveMap, LiveObject instances, for which updates are
 // automatically persisted and synced to all connected clients.
 type Storage = {
-  // author: LiveObject<{ firstName: string, lastName: string }>,
-  // ...
+  layers: LiveMap<string, LiveObject<Layer>>;
+  layerIds: LiveList<string>;
 };
 
 // Optionally, UserMeta represents static/readonly metadata on each user, as

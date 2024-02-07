ee67310b29d13dd90c7d41dc2cba6ab02523fb4a
stimw <me@stimw.com>
2024-02-07 21:26:00 +0800

    feat: liveblocks pencil

--- app/board/[boardId]/_components/cursors-presence.tsx
+++ app/board/[boardId]/_components/cursors-presence.tsx
@@ -2,9 +2,41 @@
 
 import { memo } from 'react';
 
-import { useOthersConnectionIds } from '@/liveblocks.config';
-
+import { shallow } from '@liveblocks/client';
 import { Cursor } from './cursor';
+import { useOthersConnectionIds, useOthersMapped } from '@/liveblocks.config';
+import { colorToCss } from '@/lib/utils';
+import { Path } from './path';
+
+const Drafts = () => {
+  const others = useOthersMapped(
+    (other) => ({
+      pencilDraft: other.presence.pencilDraft,
+      penColor: other.presence.penColor,
+    }),
+    shallow
+  );
+
+  return (
+    <>
+      {others.map(([key, other]) => {
+        if (other.pencilDraft) {
+          return (
+            <Path
+              key={key}
+              x={0}
+              y={0}
+              points={other.pencilDraft}
+              fill={other.penColor ? colorToCss(other.penColor) : '#000'}
+            />
+          );
+        }
+
+        return null;
+      })}
+    </>
+  );
+};
 
 const Cursors = () => {
   const ids = useOthersConnectionIds();
@@ -21,6 +53,7 @@ const Cursors = () => {
 export const CursorsPresence = memo(() => {
   return (
     <>
+      <Drafts />
       <Cursors />
     </>
   );
